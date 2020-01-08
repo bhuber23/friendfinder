58e733bd049aa2eca8c3a8f3257ae15ac28c05ad
Brittany Huber <brittanyleighhuber@gmail.com>
2020-01-07 20:24:13 -0700

    Created friends array

--- data/friends.js
+++ data/friends.js
@@ -22,4 +22,3 @@ var friends = [
 ];
 
 module.exports = friends;
-
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var path = require("path");
+
 
 var app = express();
 
@@ -9,3 +9,11 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
