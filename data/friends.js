ec1aca38549a88033354d85f78b46a0a0c049741
Brittany Huber <brittanyleighhuber@gmail.com>
2020-01-08 22:32:58 -0700

    Added to friends array and changed questions

--- data/friends.js
+++ data/friends.js
@@ -2,22 +2,37 @@ var friendsArray = [
     {
     name: "Leslie",
     photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/34/Leslie_Knope_%28played_by_Amy_Poehler%29.png/240px-Leslie_Knope_%28played_by_Amy_Poehler%29.png",
-    scores: [2, 5, 1, 3, 3, 4, 5, 5, 1, 2]
+    scores: [5, 1, 5, 2, 5, 3, 5, 2, 2, 5]
     },
     {
         name: "Ron",
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/RonSwanson.jpg/250px-RonSwanson.jpg",
-        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+        scores: [3, 5, 2, 1, 5, 1, 3, 2, 1, 5]
     },
     {
         name: "Tom",
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1f/Tom_Haverford.jpg/230px-Tom_Haverford.jpg",
-        scores: [2, 3, 5, 1, 3, 4, 4, 5, 1, 5]
+        scores: [2, 3, 4, 5, 5, 2, 3, 2, 1, 1]
     },
     {
         name: "Ann",
         photo: "https://upload.wikimedia.org/wikipedia/en/thumb/3/33/Ann_Perkins.jpg/240px-Ann_Perkins.jpg",
-        scores: [4, 5, 5, 5, 3, 1, 2, 2, 3, 4]
+        scores: [4, 2, 4, 3, 4, 2, 5, 2, 1, 2]
+    },
+    {
+        name: "Andy",
+        photo: "https://upload.wikimedia.org/wikipedia/en/4/47/Andy_Dwyer.jpg",
+        scores: [4, 3, 4, 2, 5, 5, 5, 3, 2, 3]
+    },
+    {
+        name: "April",
+        photo: "https://66.media.tumblr.com/fd8b1a516093018ec59efd2afb7d4014/tumblr_ouearsuve31vh4kswo8_250.png",
+        scores: [3, 3, 2, 2, 5, 1, 4, 5, 1, 3]
+    },
+    {
+        name: "Ben",
+        photo: "https://upload.wikimedia.org/wikipedia/en/thumb/0/02/Ben_Wyatt_Parks_%26_Rec.jpg/235px-Ben_Wyatt_Parks_%26_Rec.jpg",
+        scores: [4, 1, 5, 2, 1, 2, 5, 2, 5, 1]
     }
 ];
 
