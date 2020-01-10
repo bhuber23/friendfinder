851b4d4f4d72cefab4a896e0050cdc1fb5c8fec8
Brittany Huber <brittanyleighhuber@gmail.com>
2020-01-09 22:15:35 -0700

    Fixed bug in for loop

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -3,11 +3,29 @@ var friends = require("../data/friends");
 module.exports = function(app){
     app.get("/api/friends", function(req, res){
         res.json(friends);
-        console.log(friends);
+        //console.log(friends);
     });
 
     app.post("/api/friends", function(req, res){
-       friendsArray.push(req.body); 
-       res.json(friends);
+        var userInput = req.body.scores;
+    
+
+        //Logic for calculating best friend compatibility
+        var bestFriend;
+        var maxDifference = 41;
+
+        for(var i = 0; i < friends.length; i++){
+            var totalDiff = 0;
+            for(var j = 0; j < friends[i].scores.length; j++){
+                totalDiff += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userInput[j]));
+                
+            };
+            if (totalDiff < maxDifference){
+                maxDifference = totalDiff;
+                bestFriend = i;
+            };
+        };
+       friends.push(req.body); 
+       res.json(friends[bestFriend]);
     });
 };
